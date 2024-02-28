64cb6416de02da97ba199f24ecb7594c28b35503
Challa Rishi <challarishi02@gmail.com>
2024-02-28 11:27:07 +0530

    prac1_2

--- Prac_1_2.java
+++ Prac_1_2.java
@@ -4,14 +4,14 @@
                 double a1 = 3.4, b1 = 50.2, c1 = 44.5;
                 double a2 = 2.1, b2 = 0.55, c2 = 5.9;
         
-                double determinant = a1 * b2 - a2 * b1;
-                double xDeterminant = c1 * b2 - c2 * b1;
-                double yDeterminant = a1 * c2 - a2 * c1;
+                double equation = a1 * b2 - a2 * b1;
+                double xequation = c1 * b2 - c2 * b1;
+                double yequation = a1 * c2 - a2 * c1;
         
-                if (determinant != 0) {
+                if (equation != 0) {
                    
-                    double x = xDeterminant / determinant;
-                    double y = yDeterminant / determinant;
+                    double x = xequation / equation;
+                    double y = yequation / equation;
         
                     System.out.println("Solution for x: " + x);
                     System.out.println("Solution for y: " + y);
