41e97f8262ae11ce2dbd9f24791664243e5a3ecb
Challa Rishi <challarishi02@gmail.com>
2024-02-29 03:52:14 +0530

    Prac_2_2

---
+++ Prac_2_2.java
@@ -0,0 +1,18 @@
+public class Prac_2_2 {
+    public static void main(String[] args) {
+        int matrix[][] = {{2,41,9},{3,31,96},{10,6,32}};
+        int min = matrix[0][0];
+        int min_i=0,  min_j=0;
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]<min){
+                    min = matrix[i][j];
+                    min_i=i;
+                     min_j=j;
+                }
+            }
+        }
+        System.out.println("Minimum element is :" +min);
+    }
+}
