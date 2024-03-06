e110aeb51f48e8070794868f58ce813b7632b05b
Challa Rishi <challarishi02@gmail.com>
2024-03-06 10:26:01 +0530

    sc Prac_2_4

--- Prac_2_4.java
+++ Prac_2_4.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Prac_2_4 { 
     public static void main(String[] args) { 
         
-        Scanner scanner = new Scanner(System.in); 
+        Scanner sc = new Scanner(System.in); 
          
         
         System.out.println("Enter the elements of the first 3x3 array:"); 
-        int[][] array1 = readArray(scanner); 
+        int[][] array1 = readArray(sc); 
          
         
         System.out.println("Enter the elements of the second 3x3 array:"); 
-        int[][] array2 = readArray(scanner); 
+        int[][] array2 = readArray(sc); 
          
         
         System.out.print("Enter the symbol for the operation (+, -, or *): "); 
-        char symbol = scanner.next().charAt(0); 
+        char symbol = sc.next().charAt(0); 
          
         
         int[][] resultArray = null; 
@@ -39,15 +39,15 @@ public class Prac_2_4 {
             displayArray(resultArray); 
         } 
          
-        scanner.close(); 
+        sc.close(); 
     } 
      
 
-    public static int[][] readArray(Scanner scanner) { 
+    public static int[][] readArray(Scanner sc) { 
         int[][] array = new int[3][3]; 
         for (int i = 0; i < 3; i++) { 
             for (int j = 0; j < 3; j++) { 
-                array[i][j] = scanner.nextInt(); 
+                array[i][j] = sc.nextInt(); 
             } 
         } 
         return array; 
