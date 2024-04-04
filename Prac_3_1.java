c93c9dd0a8e4dba0598abbe50456075511dc6c6e
Challa Rishi <challarishi02@gmail.com>
2024-04-04 16:29:28 +0530

    new

---
+++ Prac_3_1.java
@@ -0,0 +1,12 @@
+public class Prac_3_1 {
+    public static void main(String[] args) {
+       int numArgs =  args.length;
+        System.out.println("Number of argument = "+numArgs);
+
+        String[] Numerics_Names = {"First", "Second", "Third", "Fourth", "Fifth"};
+
+        for(int i=0; i<num_Args; i++){
+            System.out.println((i+1)+ ":"+Numerics_Names[i]+"Student Names are : "+args[i]);
+        }
+    }
+}
